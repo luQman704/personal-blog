e57071071d323dcabe2eb94fa5abcb17a2b5016d
Lukmon Awoyemi <lukmon.awoyemi@softescu.com>
2026-05-18 11:49:27 +0100

    trigger vercel deployment

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import Hello from "@/components/Hello";
+
+const Page = () => {
+  return (
+      <div>
+        Welcome to Head
+
+          <div>
+              <Hello />
+          </div>
+      </div>
+  )
+}
+
+export default Page;
